4b7df1e7775fd3a8285eb0585f86fec381a2369f
Asobra <elias.sousa.barbosa0@gmail.com>
2024-11-05 01:01:32 +0000

    Adriano_Brendon_Elias_Rafael

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const contatoRoutes = require('./routes/contatoRoutes');
 const initDB = require('./db/initDB');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 
